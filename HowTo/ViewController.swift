aaea5fdfe909b31dd29fb850e7f4d3399674dcd9
iossenac <iossenac@sala309pc05.local>
2016-11-19 15:58:35 -0200

    Initial Commit

---
+++ HowTo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HowTo
+//
+//  Created by iossenac on 19/11/16.
+//  Copyright © 2016 HowTo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
