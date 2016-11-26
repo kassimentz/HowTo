47181fb8463c498de1e307d5a45ece9a0d90918c
“Alice <alicewiener@gmail.com>
2016-11-26 15:23:55 -0200

    adding Steps and Tutorial models. Also, adding cloud kit to the project

---
+++ HowTo/Tutorial.swift
@@ -0,0 +1,18 @@
+//
+//  Tutorial.swift
+//  HowTo
+//
+//  Created by Alice Wiener on 26/11/16.
+//  Copyright © 2016 HowTo. All rights reserved.
+//
+
+import UIKit
+
+class Tutorial: NSObject {
+    
+    var title:String?
+    var textDescription:String?
+    var image:UIImage?
+    var steps:[Steps]?
+
+}
