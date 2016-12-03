9be8251ea4218ec97feae78f979b8b53cd275d63
Kassiane Mentz <kassimentz@gmail.com>
2016-12-03 11:49:32 -0200

    create tutorialDetailCell and stepDetailCell

---
+++ HowTo/StepsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StepsTableViewCell.swift
+//  HowTo
+//
+//  Created by Kassiane Mentz on 03/12/16.
+//  Copyright © 2016 HowTo. All rights reserved.
+//
+
+import UIKit
+
+class StepsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HowTo/TutorialDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TutorialDetailTableViewCell.swift
+//  HowTo
+//
+//  Created by Kassiane Mentz on 03/12/16.
+//  Copyright © 2016 HowTo. All rights reserved.
+//
+
+import UIKit
+
+class TutorialDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
