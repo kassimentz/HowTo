a6afdffee7ff250e2d5147e61b25719d8e25da3c
kassianesmentz <kassimentz@gmail.com>
2016-12-04 12:10:19 -0200

    add search barr (but the new tutorials button vanished)

--- HowTo/MyTutorialsTableViewController.swift
+++ HowTo/MyTutorialsTableViewController.swift
@@ -8,90 +8,114 @@
 
 import UIKit
 
-class MyTutorialsTableViewController: UITableViewController {
+class MyTutorialsTableViewController: UITableViewController, UISearchBarDelegate, UISearchResultsUpdating {
 
-    @IBOutlet weak var newTutorialButton: UIButton!
+    @IBOutlet var newTutorialButton: UITableView!
+    
+    
+    // MARK: - Properties
+    var tutorials = [Tutorial]()
+    var filteredTutorials = [Tutorial]()
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        definesPresentationContext = true
+        searchController.dimsBackgroundDuringPresentation = false
+        tableView.tableHeaderView = searchController.searchBar
+        
+        
+        self.title = "Meus Tutoriais"
+        
+        populateMyTutorialsList()
+        
     }
-
+    
+    func populateMyTutorialsList() {
+        tutorials = [
+            Tutorial(title:"titulo1", textDescription: "textDescription1", image: #imageLiteral(resourceName: "Play Filled-100")),
+            Tutorial(title:"titulo2", textDescription: "textDescription2", image: #imageLiteral(resourceName: "Play Filled-100")),
+            Tutorial(title:"titulo3", textDescription: "textDescription3", image: #imageLiteral(resourceName: "Play Filled-100")),
+            Tutorial(title:"titulo4", textDescription: "textDescription4", image: #imageLiteral(resourceName: "Play Filled-100")),
+            Tutorial(title:"titulo5", textDescription: "textDescription5", image: #imageLiteral(resourceName: "Play Filled-100"))
+        ]
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredTutorials.count
+        }
+        return tutorials.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell:MyTutorialTableViewCell = tableView.dequeueReusableCell(withIdentifier: "myTutorialsCell", for: indexPath) as! MyTutorialTableViewCell
+        let tutorial: Tutorial
+        if searchController.isActive && searchController.searchBar.text != "" {
+            tutorial = filteredTutorials[indexPath.row]
+        } else {
+            tutorial = tutorials[indexPath.row]
+        }
+        
+        cell.myTutorialsTitle?.text = tutorial.title
+        cell.myTutorialsDescription?.text = tutorial.textDescription
+        cell.myTutorialsImage?.image = tutorial.image
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let tutorial: Tutorial
+        if searchController.isActive && searchController.searchBar.text != "" {
+            tutorial = filteredTutorials[indexPath.row]
+        } else {
+            tutorial = tutorials[indexPath.row]
+        }
+        performSegue(withIdentifier: "showMyTutorialDetail", sender: tutorial)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func filterContentForSearchText(_ searchText: String) {
+        filteredTutorials = tutorials.filter({( tutorial : Tutorial) -> Bool in
+            return (tutorial.title?.lowercased().contains(searchText.lowercased()))!
+        })
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    // MARK: - UISearchBar Delegate
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        filterContentForSearchText(searchBar.text!)
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        filterContentForSearchText(searchBar.text!)
     }
-    */
-
-    /*
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // Usar quando a tableViewController de MyTutorial Detalhes estiver pronta
+    /*
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "showMyTutorialDetail" {
+            let tutorialDetail = segue.destination as! MyTutorialDetailTableViewController
+            if let tutorial = sender as? Tutorial {
+                tutorialDetail.tutorial  = tutorial
+            }
+        }
     }
-    */
+     */
 
 }
