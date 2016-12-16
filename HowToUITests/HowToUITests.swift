4d02bb770b9076e6663e56cd4815742c163ba71f
Kassiane Mentz <kassimentz@gmail.com>
2016-12-16 21:50:48 -0200

    add view tutorial and steps test

--- HowToUITests/HowToUITests.swift
+++ HowToUITests/HowToUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class HowToUITests: XCTestCase {
+    
         
     override func setUp() {
         super.setUp()
@@ -31,6 +32,51 @@ class HowToUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app2 = XCUIApplication()
+        let app = app2
+        app.tables["Empty list"].swipeDown()
+        app.navigationBars["Tutoriais"].buttons["Search"].tap()
+        app2.keys["P"].tap()
+        
+        let pesquisePorTutoriaisSearchField = app.searchFields["Pesquise por tutoriais"]
+        pesquisePorTutoriaisSearchField.typeText("Pao")
+        
+        let deleteKey = app2.keys["delete"]
+        deleteKey.tap()
+        deleteKey.tap()
+        deleteKey.tap()
+        
+        let tablesQuery = app2.tables
+        tablesQuery.staticTexts["Como fazer pão de queijo"].tap()
+        
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element.children(matching: .table).element.tap()
+        
+        let vAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZafStaticText = tablesQuery.staticTexts["Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaffari Vá até o Zaf"]
+        vAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZafStaticText.swipeUp()
+        vAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZaffariVAtOZafStaticText.tap()
+        
+        let element3 = element.children(matching: .other).element
+        let element2 = element3.children(matching: .other).element(boundBy: 1)
+        let button = element2.children(matching: .button).element(boundBy: 1)
+        button.tap()
+        
+        let button2 = element3.children(matching: .button).element
+        button2.tap()
+        button2.tap()
+        button.tap()
+        app.navigationBars["Passo 3/3"].buttons[" "].tap()
+        tablesQuery.staticTexts["Siga os passos do pacote :P"].tap()
+        element2.children(matching: .button).element(boundBy: 0).tap()
+        app.navigationBars["Passo 2/3"].buttons[" "].tap()
+        
+        let button3 = app.navigationBars["Tutorial"].buttons[" "]
+        button3.tap()
+        pesquisePorTutoriaisSearchField.buttons["Clear text"].tap()
+        tablesQuery.staticTexts["Como fazer download de um livro na internet"].tap()
+        button3.tap()
+        
     }
     
 }
